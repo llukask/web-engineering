3f932e57f4c7cd732535d52944d2a91c86989d54
Lukas Kilian <lukas.kilian@outlook.at>
2017-04-20 21:18:46 +0200

    views

---
+++ lab2/app/components/details.components.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: '../views/detailsView.html'
+})
+export class DetailsComponent {
+}
---
+++ lab2/app/components/serverstats.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'server-stats',
+    templateUrl: '../views/serverStatsView.html'
+})
+export class ServerStatsComponent {
+
+}
