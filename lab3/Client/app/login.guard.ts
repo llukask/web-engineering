407059bd7ee90605b83786feb06266ab6bcef966
Lukas Kilian <kilian@dolph.in>
2017-05-21 22:41:38 +0200

    fix auth guard

--- lab3/Client/app/login.guard.ts
+++ lab3/Client/app/login.guard.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
 import {AuthService} from './services/auth.service';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class LoginGuard implements CanActivate {
 
   constructor(private router: Router, private authService: AuthService) {}
 
-  canActivate(): Promise<boolean> {
+  canActivate(): Observable<boolean> {
     if (!localStorage.getItem('currentUser')) {
       this.router.navigate(['/login']);
-      return Promise.resolve(false);
+      return Observable.of(false);
     }
 
-    var result = this.authService.checkToken(localStorage.getItem('currentUser'));
+    let result = this.authService.checkToken(JSON.parse(localStorage.getItem('currentUser')).token);
     console.log(result);
     return result;
   }
--- lab3/Client/app/services/auth.service.ts
+++ lab3/Client/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http, Headers, Response, RequestOptions} from '@angular/http';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/take'
 
 @Injectable()
 export class AuthService {
@@ -13,13 +14,16 @@ export class AuthService {
         this.token = currentUser && currentUser.token;
     }
 
-    checkToken(token: string) {
+    checkToken(token: string) : Observable<boolean> {
+        console.log("Trying to auth with token: " + token);
         let headers = new Headers({ 'token': token });
         let options = new RequestOptions({ headers: headers });
-        return Promise.resolve(this.http.get('http://localhost:8081/auth', options)
+        return this.http.get('http://localhost:8081/auth', options)
           .map((response: Response) => {
-            return response.json() && response.json().valid;
-          }).toPromise());
+            let valid = response.json() && response.json().valid;
+            console.log("Auth: " + response.text());
+            return valid;
+          });
     }
 
     login(username: string, password: string): Observable<boolean> {
